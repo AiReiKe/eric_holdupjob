64836de3537dbf9f73938544233f1becfcceca27
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2021-02-25 19:54:34 +0800

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,18 @@
+Locales['en'] = {
+  ['shop_robbery'] = 'Robbery|%s',
+  ['press_to_rob'] = 'Press ~INPUT_CONTEXT~ ~o~to rob~s~ ~b~%s~s~',
+  ['robbery_timer'] = 'Robbery %s:~r~%s~s~ second later',
+  ['recently_robbed'] = '%s has robbed。Please wait for ~y~%s~s~ seconds',
+  ['rob_in_prog'] = '~r~ Robbery is in progress, at ~b~%s~s~',
+  ['started_to_rob'] = 'You have started robbery ~y~%s~s~',
+  ['alarm_triggered'] = 'The alarm has been triggered',
+  ['robbery_complete'] = '~r~Robbery sucessful~s~, You ~o~stole~s~ ~g~$%s~s~',
+  ['robbery_complete_at'] = '~r~Robbery sucessful, at ~y~%s~s~',
+  ['robbery_cancelled'] = 'Robbery has been cancelled!',
+  ['robbery_cancelled_at'] = 'Robbery ~r~at ~b~%s~s~ has been cancelled!',
+  ['min_police'] = 'At least need ~b~%s police~s~ online that you can rob.',
+  ['robbery_already'] = '~r~Robbery is in progress.',
+  ['no_threat'] = 'You will not be threat to ~b~%s~w~',
+  ['society_empty'] = 'Robbery failed: This company is out of money',
+  ['min_member'] = 'At least need ~b~%s %s member~s~ online that you can rob.',
+}
--- locales/tw.lua
+++ locales/tw.lua
@@ -1,1 +1,18 @@
-
+Locales['tw'] = {
+  ['shop_robbery'] = '搶劫|%s',
+  ['press_to_rob'] = '按 ~INPUT_CONTEXT~ 去 ~o~搶劫~s~ ~b~%s~s~',
+  ['robbery_timer'] = '%s搶劫:剩餘 ~r~%s~s~ 秒',
+  ['recently_robbed'] = '%s最近被搶劫了。請等 ~y~%s~s~ 秒',
+  ['rob_in_prog'] = '~r~ 搶劫正在進行,在 ~b~%s~s~',
+  ['started_to_rob'] = '你已開始搶劫 ~y~%s~s~',
+  ['alarm_triggered'] = '警報已被觸發',
+  ['robbery_complete'] = '~r~搶劫已經成功了~s~, 你 ~o~偷了~s~ ~g~$%s~s~',
+  ['robbery_complete_at'] = '~r~搶劫成功,在 ~y~%s~s~',
+  ['robbery_cancelled'] = '搶劫已被取消!',
+  ['robbery_cancelled_at'] = '~r~在 ~b~%s~s~ 的搶劫已被取消!',
+  ['min_police'] = '至少要有 ~b~%s 個警察~s~ 才能搶劫.',
+  ['robbery_already'] = '~r~搶劫已經在進行中.',
+  ['no_threat'] = '你不會對~b~%s~w~構成威脅',
+  ['society_empty'] = '搶劫失敗: 這間公司沒錢了',
+  ['min_member'] = '至少要有 ~b~%s 個%s成員~s~ 才能搶劫.',
+}
